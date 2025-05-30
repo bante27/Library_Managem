7c358e9d46091aefeeca5f42536757daa024e470
bantalem mitiku <mitikubantalem@gmail.com>
2025-05-30 16:58:14 -0400

    Delete SQLQuery8.sql

--- SQLQuery8.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE users (
-    Id INT PRIMARY KEY IDENTITY(1,1), -- Auto-incrementing unique ID
-    Username NVARCHAR(50) NOT NULL UNIQUE, -- Usernames must be unique and not null
-    Password NVARCHAR(255) NOT NULL, -- Storing hashed passwords is best practice, but for now NVARCHAR is fine
-    Email NVARCHAR(100) NOT NULL UNIQUE, -- Email must be unique and not null
-    date_register DATE DEFAULT GETDATE() -- Automatically sets the registration date
-);
